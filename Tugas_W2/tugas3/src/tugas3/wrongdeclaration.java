e773dee71e30e697792665bef972231e901c37f4
M-Ilham-Nugraha-Syahputra <80625504+M-Ilham-Nugraha-Syahputra@users.noreply.github.com>
2021-03-16 20:20:12 +0700

    Add files via upload

---
+++ Tugas_W2/tugas3/src/tugas3/wrongdeclaration.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class wrongdeclaration {
+
+    boolean gameOver = false; //benar
+    int students = 50, classes = 3; //benar
+    double sales_tax; //benar
+    short number1; //benar
+    
+    int 2beOrNot2be; 
+    
+    float price index;
+    
+    double lastYear'Price;
+   
+    long class;
+   
+    
+}
